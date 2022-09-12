1012f8c2eb371356284f7d7c0c9ca9616a071eab
Saqib Amin <saqibamin257@gmail.com>
2022-09-12 17:53:40 +0100

    initial commit

---
+++ Services/CategoryService.cs
@@ -0,0 +1,13 @@
+﻿using Supermarket.API.Domain.Models;
+using Supermarket.API.Domain.Services;
+
+namespace Supermarket.API.Services
+{
+    public class CategoryService :ICategoryService
+    {
+        public async Task<IEnumerable<Category>> ListAsync() 
+        {
+
+        }
+    }
+}
