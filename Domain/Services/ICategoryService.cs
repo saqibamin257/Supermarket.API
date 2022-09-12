087773eee867f117d61d4ad5fbb75f39ab8265df
Saqib Amin <saqibamin257@gmail.com>
2022-09-12 17:40:39 +0100

    Add project files.

---
+++ Domain/Services/ICategoryService.cs
@@ -0,0 +1,9 @@
+﻿using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Domain.Services
+{
+    public interface ICategoryService
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
