8bcf72bf2af9342f3c7920fb614f8c6c67a23c30
Saqib Amin <saqibamin257@gmail.com>
2022-09-17 10:30:18 +0100

    added DB Context class and other interface. creating architecture of the application

---
+++ Domain/Repositories/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using Supermarket.API.Domain.Models;
+
+namespace Supermarket.API.Domain.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> ListAsync();
+    }
+}
